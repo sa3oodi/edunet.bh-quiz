56d1e55e4185dd96a07474aeb5822c4af2ecc7df
Hasan Al-Naham <66477177+imcr1@users.noreply.github.com>
2021-04-01 12:25:42 +0300

    js answer with essay v2.0

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ async function edunet_request(link) {
         return response
     }))
 }
+
+
+
+
+
+
 async function edunet_quize(ID, Course_Id) {
     return fetch('https://www.edunet.bh/Quiz/GetQuizQuestions', {
         method: 'POST',
@@ -37,17 +43,23 @@ async function edunet_quize(ID, Course_Id) {
 }
 var source_of_current_page = document.documentElement.innerHTML
 get_id_of_quize(source_of_current_page).then(quize_id => {
-        get_id_of_cource(source_of_current_page).then(cource_id => {
-            edunet_quize(quize_id, cource_id).then(respon => {
-                respon.forEach(element => {
+    get_id_of_cource(source_of_current_page).then(cource_id => {
+        edunet_quize(quize_id, cource_id).then(respon => {
+            respon.forEach(element => {
+                var qtype = element['QuestionTypeID']
+                if (qtype === 3) {
                     var choice = element['ChoicesList']
                     choice.forEach(ans => {
                         var is_correct = ans['Correct_Choice']
                         if (is_correct === true) {
-                            alert(`Question : ${removeTags(element['Question_Text']) }\nAnswer : ${ans['ChoiceText']}\n\nQuiz cheat by S.SAEED (:`)
+                            alert(`Question : ${removeTags(element['Question_Text'])}\nAnswer : ${ans['ChoiceText']}\n\nQuiz cheat by BDT (:`)
                         }
                     });
-                });
-            })
+                }else if(qtype === 3){
+                    alert(`Question : ${removeTags(element['Question_Text'])}\nAnswer : ${removeTags(element['EssayAnswer'])}\n\nQuiz cheat by BDT (:`)
+                }
+
+            });
         })
     })
+})
